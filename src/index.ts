133af7ea9d2da7d9aaafedbfcaf720229ae0084f
GabrielSIlvaRS <gabrielkko01@gmail.com>
2025-08-08 19:50:56 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,24 @@
- 
-const sociedade = ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Gimli', 'Merry', 'Pippin', 'Aragorn', 'Boromir'];
-
-for (let i = 0; i < sociedade.length; i++) {
-  if (
-    sociedade[i] === 'Frodo' ||
-    sociedade[i] === 'Sam' ||
-    sociedade[i] === 'Merry' ||
-    sociedade[i] === 'Pippin'
-  ) {
-    console.log(`Hobbit encontrado: ${sociedade[i]}\n`);
-  }
+function alunoI(nomeAlunos:string, IdadeAlunos:number, matriculado:boolean):void{
+    console.log(`Aluno ${nomeAlunos}. Idade ${IdadeAlunos}, está matriculado? ${matriculado}`)
 }
+alunoI(`Gabriel`, 22, true);
 
-type personagens ={
-  nome:string;
-  jedi:boolean;
-}
-
-const personagens = [
-  { nome: 'Luke Skywalker', jedi: true },
-  { nome: 'Leia Organa', jedi: false },
-  { nome: 'Yoda', jedi: true },
-  { nome: 'Darth Vader', jedi: false },
-];
-
-for(const personagem of personagens){
-    if(personagem.jedi === true){
-      console.log(`${personagem.nome}: é Jedi.\n`)
-    }else{
-      console.log(`${personagem.nome}: não é jedi.\n`)
+let alunos:string[] = ['Ana', 'Gabriel', 'Giovanni', 'Rodolfo', 'Pato']
+function listaAlunos(alunos:string[]):string{
+    let lista:string = 'Alunos';
+    for(let i = 0; i < lista.length; i++){
+        lista += `\n ${alunos[i]}`
     }
+    return lista
 }
+console.log(listaAlunos(alunos));
 
-const personagensn = {
-  Naruto: 'Uzumaki',
-  Sasuke: 'Uchiha',
-  Sakura: 'Haruno',
-  Kakashi: 'Hatake'
-};
-
-for (const personagem in personagensn) {
-  console.log(`${personagem} pertence ao clã ${personagensn[personagem as keyof typeof personagensn]}\n`);
+function saudacao(nome:string):void{
+    console.log(`Olá, ${nome}. Seja Bem-vindo á turma.`);
 }
+saudacao("Gabriel");
 
-const personagensg = [
-  { nome: 'Goku', poder: 15000 },
-  { nome: 'Vegeta', poder: 14999 },
-  { nome: 'Krillin', poder: 7500 },
-  { nome: 'Freeza', poder: 20000 },
-];
-
-personagensg.forEach((personagem, i) =>{
-  if(personagem.poder >= 8000){
-    console.log(`O poder de ${personagem.nome} é mais de 8000\n`);
-  }
-});
-
-
-let pokemons:string[] = ['Pikachu', 'Charmander', 'Bulbasaur', 'Mewtwo', 'Squirtle'];
-let contador = 0;
-
-while(contador < pokemons.length){
-  if(pokemons[contador] === 'Mewtwo'){
-    console.log(`Pokemo raro capturado ${pokemons[contador]}`)
-  }else{
-    console.log(`Pokemo capturado ${pokemons[contador]}`)
-  }
-  contador++
+function calcualar(nota1:number, nota2:number , nota3:number):number{
+    return (nota1 + nota2 + nota3) / 3;
 }
-
-
-let passos = 0;
-const passosParaMordor = 5;
-
-do{
-  if(passos === passosParaMordor){
-    console.log(`Passos dados ${passos}. Parabéns, você chegou a Mordor`);
-  }else{
-    console.log(`Passos dados ${passos}`)
-  }
-  passos++
-}while(passos < 6)
+console.log(`Nota final ${calcualar(10, 10, 10)}`);
